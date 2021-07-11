89de55690d9db548339332f38f4f47d250e4c2f6
Michael Oliver <michael.oliver@buffalogrid.com>
2021-07-11 19:26:59 +0100

    Clean up config

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,45 @@
 import preprocess from "svelte-preprocess";
 import { mdsvex } from "mdsvex";
+import {
+  createShikiHighlighter,
+  runTwoSlash,
+  renderCodeToHTML,
+} from "shiki-twoslash";
+import { readFileSync } from "fs";
 
-import mdsveConfig from "./mdsvex.config.cjs";
+// From https://github.com/whizkydee/vscode-palenight-theme/blob/master/themes/palenight.json
+const palenightTheme = JSON.parse(readFileSync("./palenight-theme.json"));
+
+const mdsvexPreprocess = mdsvex({
+  extensions: [".svx", ".md"],
+  highlight: {
+    highlighter: async (code, lang, meta) => {
+      const highlighter = await createShikiHighlighter({
+        theme: palenightTheme,
+      });
+
+      let twoslashResults = null;
+      if (meta && meta.includes("twoslash")) {
+        twoslashResults = runTwoSlash(code, lang, {});
+      }
+
+      const html = renderCodeToHTML(
+        code,
+        lang,
+        meta || [],
+        {},
+        highlighter,
+        twoslashResults
+      );
+
+      return `{@html \`${html}\` }`;
+    },
+  },
+});
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: [mdsvex(mdsveConfig), preprocess()],
+  preprocess: [mdsvexPreprocess, preprocess()],
   extensions: [".svelte", ".svx"],
   kit: {
     target: "#svelte",
