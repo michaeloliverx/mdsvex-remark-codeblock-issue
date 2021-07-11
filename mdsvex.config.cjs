6366e9e4f3f7ecca1f78334d873973117ccfcd1d
Michael Oliver <michael.oliver@buffalogrid.com>
2021-07-11 19:16:27 +0100

    Working

--- mdsvex.config.cjs
+++ mdsvex.config.cjs
@@ -1,12 +1,37 @@
-const remarkShikiTwoslash = require("remark-shiki-twoslash").default;
+const {
+  createShikiHighlighter,
+  runTwoSlash,
+  renderCodeToHTML,
+} = require("shiki-twoslash");
 
 // From https://github.com/whizkydee/vscode-palenight-theme/blob/master/themes/palenight.json
 const palenightTheme = require("./palenight-theme.json");
 
 const config = {
   extensions: [".svx", ".md"],
-  remarkPlugins: [[remarkShikiTwoslash, { theme: palenightTheme }]],
-  highlight: false,
+  highlight: {
+    highlighter: async (code, lang, meta) => {
+      const highlighter = await createShikiHighlighter({
+        theme: palenightTheme,
+      });
+
+      let twoslashResults = null;
+      if (meta && meta.includes("twoslash")) {
+        twoslashResults = runTwoSlash(code, lang, {});
+      }
+
+      const html = renderCodeToHTML(
+        code,
+        lang,
+        meta || [],
+        {},
+        highlighter,
+        twoslashResults
+      );
+
+      return `{@html \`${html}\` }`;
+    },
+  },
 };
 
 module.exports = config;
